e37121962035595e90cfa9e75840c33e2b60afd8
Thomas Castleman <thomas7rex@gmail.com>
2019-05-19 21:49:40 -0400

    Finish search funcs

--- search.js
+++ search.js
@@ -32,23 +32,41 @@ module.exports = {
 		If numObjects given, filter to include only patterns with that number of objects. 
 		Empty query returns full table */
 	searchPatterns: function(query, numObjects, limit, cb) {
+		// ensure limit exists and that numObjects is positive if given
+		if (limit && limit > 0 && (!numObjects || numObjects > 0)) {
+			// add wildcard to search query to broaden search
+			var query = query == "" ? query : query + "*";
 
-	},
+			// determine if we should filter by number of objects
+			var notFiltering = numObjects == undefined;
 
-	// get the maximum difficulty value out of all existing patterns
-	getMaxDifficulty: function(cb) {
+			// select all patterns that match against query
+			con.query('SELECT name, description, numObjects, GIF, difficulty, MATCH (name, description) AGAINST (? IN BOOLEAN MODE) AS termScore FROM patterns WHERE (MATCH (name, description) AGAINST (? IN BOOLEAN MODE) OR ? = "") AND (numObjects = ? OR ?) ORDER BY termScore DESC LIMIT ?;', [query, query, query, numObjects, notFiltering, limit], function(err, patterns) {
+				if (!err && patterns !== undefined) {
+					// determine the maximum difficulty out of all patterns
+					con.query('SELECT MAX(difficulty) AS max FROM patterns;', function(err, rows) {
+						if (!err && rows !== undefined && rows.length > 0) {
+							var maxDiff = rows[0].max;
 
-	},
+							// scale all difficulties out of 10
+							for (var i = 0; i < patterns.length; i++) {
+								console.log(patterns[i].difficulty);
+								patterns[i].difficulty = 10 * patterns[i].difficulty / maxDiff;
+							}
 
-	// scale a difficulty out of 10
-	scaleOutOfTen(maxDifficulty, difficulty) {
-		return 10 * (difficulty) / maxDifficulty;
+							// callback on patterns array
+							cb(err, patterns);
+						} else {
+							cb(err);
+						}
+					});
+				} else {
+					cb(err || "The system was unable to retrieve pattern search results.");
+				}
+			})
+		} else {
+			cb("Invalid results limit or number of objects filter given.");
+		}
 	}
 
 }
-
-
-// module.exports.searchUsers('', -1, function(err, users) {
-// 	if (err) throw err;
-// 	console.log(users);
-// });
