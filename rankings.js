d1909a16a4613a81e270a4d9fb845c673c4df449
Thomas Castleman <thomas7rex@gmail.com>
2019-05-21 00:19:13 -0400

    Add more pseudo for calculating record scores

--- rankings.js
+++ rankings.js
@@ -7,18 +7,29 @@ var con = require('./database.js').connection;
 
 module.exports = {
 
-	// calculate all global user scores
-	calcAllUserScores: function(cb) {
+	// calculate global user scores on a subset of users, or all users if no subset specified
+	calcUserScores: function(userUIDs, cb) {
+		// query constraint to limit records to subset of users
+		var constraint = "";
+
+		// if only calculating for a subset of users
+		if (userUIDs && userUIDs.length > 0) {
+			constraint = " AND r.userUID IN (" + userUIDs.join(',') + ")";
+		}
+
 		// get the score of each users best records,
-		con.query('SELECT r.userUID, r.score, p.difficulty as patternDifficulty FROM records r JOIN patterns p ON r.patternUID = p.uid WHERE r.isPersonalBest = 1 ORDER BY r.userUID;', [], function(err, rows){
+		con.query('SELECT r.userUID, r.score, p.difficulty as patternDifficulty FROM records r JOIN patterns p ON r.patternUID = p.uid WHERE r.isPersonalBest = 1' + constraint + ' ORDER BY r.userUID;', [], function(err, rows){
 			// if there isn't an sql error
-			if(!err && rows != undefined){
-				//if there is some iformation
-				if(rows.length > 0){
+			if (!err && rows != undefined){
+				// records do exist for these users
+				if (rows.length > 0){
 					
-					//----------------------------------//
-					//		NEEDS IMPLEMENTATION		//
-					//----------------------------------//
+
+					// !!!!!!!!!!!!!!
+					// MAKE SURE WE USE THE RECORD SCORE OF THIS USER'S MOST RECENT PERSONAL BEST (there are two PB's per event possible, one for catches, one for duration--use the more recent when calculating user score)
+					// !!!!!!!!!!!!!!
+
+
 					/*
 						this will add up all of the user's scores and then insert them into the db.
 						
@@ -68,26 +79,50 @@ module.exports = {
 						
 					*/
 
+				} else {
+					cb("There are no records from which to calculate user scores.");
 				}
-				else{
-					cb("There are no records to calculate user scores from.");
-				}
-			}
-			else{
+			} else {
 				cb("There was an error with retrieval of record information.");
 			}
 		});
 	},
 
-	// calculate global user scores for a subset of users, by UID
-	calcUserScores: function(userUIDs, cb) {
-		// Definitely combine this func with calcAllUserScores, have the userUIDs param be optional, if null, then calc all scores, if exists, calc only subset
-		// (will have to modify initial query that gets records to only select for a subset of user UIDs)
-	},
+	// calculate the record scores for all PB records within a given subset of patterns
+	calcRecordScores: function(patternUIDs, cb) {
+		// query constraint to limit records to subset of patterns
+		var constraint = "";
 
-	// calculate the record scores for all PB records within a given pattern
-	calcRecordScores: function(patternUID, cb) {
+		// if only calculating for a subset of patterns
+		if (patternUIDs && patternUIDs.length > 0) {
+			constraint = " AND patternUID IN (" + patternUIDs.join(',') + ")";
+		}
 
+		/*
+			'SELECT * FROM records WHERE isPersonalBest = 1' + constraint + ' ORDER BY patternUID, catches DESC, duration DESC;'
+				
+				This gets all the relevant PB records for these patterns, grouped off by pattern, and within that,
+				ordered catch records (best to worst) come first, then ordered duration records (best to worst)
+
+			
+			for i from 0 to records.length
+				j = i
+
+				if records[i].catches NOT null
+					while records[j].catches also NOT null:
+						records[j].score = records[j].catches / records[i].catches
+						j++
+
+				i = j
+
+				if records[i].duration NOT null
+					while records[j].duration also NOT null:
+						records[j].score = toSec(records[j].duration) / toSec(records[i].duration)
+						j++
+
+				i = j
+
+		*/
 	},
 
 	// calculate the difficulties of a subset of patterns
@@ -123,13 +158,14 @@ module.exports = {
 			Callback on any errors, and both values
 			cb(err, maxAvgCatch, maxAvgTime)
 		*/
-
 	},
 
 	// determine the more popular scoring method (time- or catch-based) for a given pattern
 	determinePopularScoringMethod: function(patternUID, cb) {
 		/*
-			
+			SELECT isTimeRecord, COUNT(*) AS count FROM (SELECT catches IS NULL AS isTimeRecord FROM records WHERE patternUID = ?) AS types GROUP BY isTimeRecord;
+
+			Use this to callback on which method has higher count of records
 		*/
 	}
 
@@ -173,4 +209,22 @@ module.exports = {
 
 	*/
 
+}
+
+// convert a duration string into an integer number of seconds, for easy comparison
+function toSec(duration) {
+	// split duration string into components
+	var spl = duration.split(':');
+
+	if (spl.length > 2) {
+		// parse hours, minutes, seconds
+		var hr = parseInt(spl[0]);
+		var min = parseInt(spl[1]);
+		var sec = parseInt(spl[2]);
+
+		// convert to seconds
+		return (hr * 3600) + (min * 60) + sec;
+	} else {
+		return null;
+	}
 }
